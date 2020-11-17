e38c40c678255e7e12e494d5ca70af5eef67346b
Bowen Gu <74045912+Jack24488@users.noreply.github.com>
2020-11-17 18:42:38 +0800

    Add files via upload

---
+++ Untitled7.m
@@ -0,0 +1,38 @@
+function grade=Untitled7()
+score=input('请输入卷面成绩:score=');
+switch fix(score/10)     
+    case 10
+        grade='A';
+    case 9
+        grade='A';
+    case 8
+        grade='B';
+    case 7
+        grade='C';
+    case 6
+        grade='D';
+    case 5
+        grade='E';
+    case 4
+        grade='E';
+    case 3
+        grade='E';
+    case 2
+        grade='E';
+    case 1
+        grade='E';
+    case 0
+        grade='E';
+    otherwise
+        grade='成绩不合理';
+end
+        
+    
+        
+
+
+
+  
+  
+  
+
